db4fb9f8b9e27962418019e533965f5fc7dfb814
dmorad1004 <dmorad@unal.edu.co>
2020-12-13 21:58:32 -0500

    chat finalizado

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -22,19 +22,25 @@ io.on('connection', (client) => {
 
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
-        client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('administrador', `${data.nombre} se unió`));
+
 
         callback(usuarios.getPersonasPorSala(data.sala));
 
     })
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         let persona = usuarios.getPersona(client.id);
 
-        let mensaje = crearMensaje(persona, data.mensaje);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+
 
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+
+        callback(mensaje);
 
     })
 
@@ -44,7 +50,7 @@ io.on('connection', (client) => {
         let personaBorrada = usuarios.borrarPersona(client.id);
 
         client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('administrador', `${personaBorrada.nombre} salió`));
-        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
 
     })
 
